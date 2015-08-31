4c4a8893fc485f5d6af611e3a8bd55b793cc149b
johnrhampton <johnrhampton@gmail.com>
2015-08-31 12:53:50 -0400

    env dev server fix for deploy

--- config/environments.conf.js
+++ config/environments.conf.js
@@ -16,7 +16,7 @@ module.exports = {
     dev: {
       'environment': {
         'ENV_TYPE': 'dev',
-        'API_PATH': 'https://someotherserver.com',
+        'API_PATH': 'https://mean-ui.herokuapp.com/',
         'VERSION': 'v ' + pkg.version
       }
     }
